e35735c63c2d9fe9d561ac5398b727edcf50cc33
Matheustj35 <157541364+Matheustj35@users.noreply.github.com>
2025-10-03 20:31:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Botão "Voltar ao topo"
+const btnTopo = document.createElement('button');
+btnTopo.innerText = "↑";
+btnTopo.id = "btnTopo";
+document.body.appendChild(btnTopo);
+
+btnTopo.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+window.addEventListener('scroll', () => {
+  btnTopo.style.display = window.scrollY > 400 ? 'block' : 'none';
+});
 
